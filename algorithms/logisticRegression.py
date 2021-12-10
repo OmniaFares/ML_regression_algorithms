ca810a353ea6ebc6eb01a337d220d39e0b0a3312
OmniaFares <omniafares133@gmail.com>
2021-12-10 20:30:35 +0200

    logistic regression after normalization

--- algorithms/logisticRegression.py
+++ algorithms/logisticRegression.py
@@ -8,9 +8,9 @@ from sklearn.linear_model import LogisticRegression
 data = pd.read_csv('heart.csv')
 x = np.c_[data['trestbps'], data['chol'], data['thalach'], data['oldpeak']]
 y = data['target']
+x = (x - x.mean()) / (x.std())
 
-
-percent = (int)(y.size * 0.8)
+percent = int(len(x) * 0.8)
 x_train = x[:percent]
 x_train = np.c_[np.ones(x_train.shape[0]), x_train]
 y_train = np.array(y[:percent])
@@ -21,21 +21,29 @@ y_test = np.array(y[percent:])
 
 
 theta = np.zeros(np.size(x, 1) + 1)
-alpha = 0.1
+alpha = 0.01
 iterations = 1000
 
 
 def costfunction(theta,x,y):
     h = hypothesis(x,theta)
-    predictions1 = -y * np.log(h)
-    predictions2 = (1 - y) * np.log(1-h)
-    errors = (predictions1 - predictions2)
-    J = 1 / (y.size) * sum(errors)
+    #print("h : ",h)
+    predictions1 = y*np.log(h)
+    #print(np.log(h))
+    #print(predictions1)
+    predictions2 = (1-y) * np.log(1-h)
+    #print(np.log(1-h))
+    #print(predictions2)
+    errors = (predictions1 + predictions2)
+    #print(errors)
+    J = (-1 / (y.size)) * np.sum(errors)
+    print(J)
     return J
 
 
 def hypothesis(x, theta):
     ex = np.dot(x, theta)
+    #print(ex, "jjjjjj ", np.exp(-ex))
     return 1 / (1 + np.exp(-ex))
 
 
@@ -47,14 +55,17 @@ def gradient_descent(x, y, theta, iterations, alpha):
         j = costfunction(theta, x, y)
         past_costs.append(j)
         diff = np.dot(x.T, h - y)
+       # print(diff)
         theta = theta - alpha * diff
+       # print(theta)
         past_thetas.append(theta)
 
     return theta, past_costs, past_thetas
 
 
 def predict(x, y_test, theta):
-    y_predict = hypothesis(x, theta)
+    h = hypothesis(x, theta)
+    y_predict = np.where(h >= .5, 1, 0)
     print(y_test, y_predict)
     MSE = (1 / y.size) * np.sum(((y_predict - y_test) ** 2))
     acc = np.sum(np.equal(y_test, y_predict)) / len(y_predict)
@@ -63,6 +74,7 @@ def predict(x, y_test, theta):
 
 theta, past_costs, past_thetas = gradient_descent(x_train, y_train, theta, iterations, alpha)
 print(theta)
+
 
 acc, MSE = predict(x_test, y_test, theta)
 print("error = ", MSE)
